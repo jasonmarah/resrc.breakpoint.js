1eb8de436f4e2f493879a3893f1c8ca0933d4c64
Dom Fee <domfee@gmail.com>
2014-05-04 01:16:03 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# ReSRC "Art Direction" Plugin
+
+In a responsive design, it is typical to change an image so it can be targeted towards the features of a particular display (or set of displays). Sometimes this means cropping an image. Other times, it can mean a different image altogether that may have different proportions or may be changed in other ways to communicate more effectively in a layout.
+
+resrc.breakpoint.js is a lightweight image "art direction" plugin for use with [resrc.js](https://github.com/resrcit/resrc.js). It bring art direction to the existing ```<img />``` tag with super clean syntax. The plugin adopts a mobile first approach, and works on all browsers that support [matchmedia](http://caniuse.com/matchmedia). Unsupported browsers fall-back gracefully to your largest image set in the breakpoint attributes. The plugin supports any sizing unit (px, em etc.) that match media understands. 
+
+## Usage
+
+1. Include [resrc.js](http://use.resrc.it/0.7):
+
+  ```html
+  <script src="//use.resrc.it/0.7"></script>
+  ```
+
+2. Include the plugin:
+
+  ```html
+  <script src="/dist/resrc.breakpoint.min.js"></script>
+  ```
+
+3. Add the appropriate data-src-[width] attribute to your image tag
+
+  ```html
+  <img data-src-0px="http://app.resrc.it/s=w320/o=85/http://www.resrc.it/img/demo/preferred.jpg"  
+       data-src-420px="http://app.resrc.it/s=w420/o=85/http://www.resrc.it/img/demo/preview.jpg"  
+       data-src-720px="http://app.resrc.it/s=w720/o=85/http://www.resrc.it/img/demo/hidpi.jpg"  
+       data-src-1024px="http://app.resrc.it/s=w1024/o=85/http://www.resrc.it/img/demo/css.jpg"  
+       alt="" class="resrc--breakpoint"
+  />
+  ```
+
+4. Call the plugin:
+
+  ```javascript
+  resrc.ready(function () {
+    resrc.breakpoint();
+  });
+  ```
+  
+4. Or call the plugin and set your own defaults:
+
+  ```javascript
+  resrc.ready(function () {
+    resrc.breakpoint({breakpointClass : "artdirection", prefix : "breakpoint-src-"});
+  });
+  ```
+      
+## Examples:
+
+* [Basic](https://github.com/resrcit/resrc.breakpoint.js/blob/master/examples/example.html)
+* [Advanced](https://github.com/resrcit/resrc.breakpoint.js/blob/master/examples/example-advanced.html)
+   
+## Building a minified release
+
+The repository does not contain a minified resrc.breakpoint.min.js file - this is only generated
+for [RELEASES](https://github.com/resrcit/resrc.breakpoint.js/releases). To build your own minified copy
+for use in development simply run ```npm install``` if you haven't already, followed by ```grunt build```.
+This will generate a resrc.breakpoint.min.js file in the `dist` subdirectory.
+
+## History
+
+For a full list of releases and changes please see the [CHANGELOG](https://github.com/resrcit/resrc.breakpoint.js/blob/master/CHANGELOG.md).
+
+## Contributing
+
+Please see the [CONTRIBUTING](https://github.com/resrcit/resrc.breakpoint.js/blob/master/CONTRIBUTING.md) file for guidelines.
+
+## Contact
+
+Please get in touch via: [EMAIL](mailto:support@resrc.it).
+
+## License
+
+Copyright (C) 2014 by [ReSRC LTD](http://www.resrc.it) - The MIT License (MIT)  
+Please see [LICENSE](https://github.com/resrcit/resrc.breakpoint.js/blob/master/LICENSE).
---
+++ src/resrc.breakpoint.js
@@ -0,0 +1,91 @@
+(function (resrc) {
+  "use strict";
+  resrc.breakpoint = function (options) {
+
+    var defaults = {
+      breakpointClass : "resrc--breakpoint",
+      prefix : "data-src-"
+    };
+
+    var settings = resrc.extend(defaults, options);
+    var imgs = resrc.getElementsByClassName(settings.breakpointClass);
+    var regex = new RegExp("^" + settings.prefix + ".*");
+
+    var sortObjectByWidth =  function (obj1, obj2) {
+      return obj1.width - obj2.width;
+    };
+
+    var setElementSrc = function () {
+      // If an element is found.
+      if (imgs) {
+        // Loop over each element.
+        for (var i = 0; i < imgs.length; i++) {
+          // Create a sources array. This will store objects containing a width, unit and a src.
+          var sources = [];
+          // Create a matches array. This will store source objects matched against the matchMedia.
+          var matches = [];
+          // Create a reference to the current element.
+          var elem = imgs[i];
+          // Loop over each attribute of the element.
+          for (var j = 0; j < elem.attributes.length; j++) {
+            // Create a reference for the current attribute.
+            var elemAttr = elem.attributes[j];
+            // If the current attributes name contains the 'settings.prefix'.
+            if (regex.test(elemAttr.nodeName)) {
+              // Add an object containing width, unit and src and add it to the sources array.
+              sources.push(
+                { "width" : elemAttr.nodeName.replace(settings.prefix, "").replace(/\D/g, ""),
+                  "unit"  : elemAttr.nodeName.replace(settings.prefix, "").replace(/[0-9]/g, ""),
+                  "src"   : elemAttr.nodeValue }
+              );
+            }
+          }
+          // Sort the sources array in ascending width order.
+          sources = sources.sort(sortObjectByWidth);
+          // Loop over the source array.
+          for (var k = 0; k < sources.length; k++) {
+            // If matchMedia is supported & matchMedia matches.
+            if (window.matchMedia && window.matchMedia("(min-width: " + sources[k].width + sources[k].unit + ")").matches) {
+              // Add the sources object to the matches array.
+              matches.push(sources[k]);
+            }
+            // If matchMedia is not supported
+            if (!window.matchMedia) {
+              // Add the last sources object to the match array
+              // Break out of the loop
+              matches.push(sources[sources.length - 1]);
+              break;
+            }
+          }
+          // If there is a match.
+          if (matches.length) {
+            // Create a reference to last matched object.
+            var matchedEl = matches.pop();
+            // If the src of the element is NOT the same as the matchedEl.src
+            if (matchedEl.src !== elem.getAttribute("data-src")) {
+              // Set the data-src of the element to the src of the matchedEl object.
+              elem.setAttribute("data-src", matchedEl.src);
+              // Set the element src by calling the resrc.getResrcImageObject public method.
+              elem.src = resrc.getResrcImageObject(elem).resrcImgPath;
+              // Remove the width and height from the element.
+              elem.removeAttribute("width");
+              elem.removeAttribute("height");
+            }
+          }
+        }
+      }
+    };
+
+    // Add a resize event listener so it runs when the browser window size changes.
+    if (window.addEventListener) {
+      window.addEventListener("resize", setElementSrc, false);
+    }
+
+    // Execute the setElementSrc function.
+    setElementSrc();
+
+  };
+
+  return resrc;
+
+}(resrc));
