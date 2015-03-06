ce200e999b2485af279fd9dfb89808143949a32d
Jason Marah <jasonmarah@gmail.com>
2015-03-06 16:49:33 +0000

    expose setElementSrc; allow setElementSrc to target individual elements; refactor setElementSrc to split main logic into processElements function

--- src/resrc.breakpoint.js
+++ src/resrc.breakpoint.js
@@ -19,65 +19,71 @@
       this.src = resrc.getResrcImageObject(this).fallbackImgPath;
     };
 
-    var setElementSrc = function () {
+    var processElements = function(elem) {
+      // Create a sources array. This will store objects containing a width, unit and a src.
+      var sources = [];
+      // Create a matches array. This will store source objects matched against the matchMedia.
+      var matches = [];
+      // Loop over each attribute of the element.
+      for (var i = 0; i < elem.attributes.length; i++) {
+        // Create a reference for the current attribute.
+        var elemAttr = elem.attributes[i];
+        // If the current attributes name contains the 'settings.prefix'.
+        if (regex.test(elemAttr.nodeName)) {
+          // Add an object containing width, unit and src and add it to the sources array.
+          sources.push({
+            "width": elemAttr.nodeName.replace(settings.prefix, "").replace(/\D/g, ""),
+            "unit": elemAttr.nodeName.replace(settings.prefix, "").replace(/[0-9]/g, ""),
+            "src": elemAttr.value
+          });
+        }
+      }
+      // Sort the sources array in ascending width order.
+      sources = sources.sort(sortObjectByWidth);
+      // Loop over the source array.
+      for (var j = 0; j < sources.length; j++) {
+        // If matchMedia is supported & matchMedia matches.
+        if (window.matchMedia && window.matchMedia("(min-width: " + sources[j].width + sources[j].unit + ")").matches) {
+          // Add the sources object to the matches array.
+          matches.push(sources[j]);
+        }
+        // If matchMedia is not supported
+        if (!window.matchMedia) {
+          // Add the last sources object to the match array
+          // Break out of the loop
+          matches.push(sources[sources.length - 1]);
+          break;
+        }
+      }
+      // If there is a match.
+      if (matches.length) {
+        // Create a reference to last matched object.
+        var matchedEl = matches.pop();
+        // If the src of the element is NOT the same as the matchedEl.src
+        if (matchedEl.src !== elem.getAttribute("data-src")) {
+          // Set the data-src of the element to the src of the matchedEl object.
+          elem.setAttribute("data-src", matchedEl.src);
+          // Set the element src by calling the resrc.getResrcImageObject public method.
+          elem.src = resrc.getResrcImageObject(elem).resrcImgPath;
+          // Fallback gracefully to the remote image if there is an error.
+          elem.onerror = useFallbackImgPath;
+          // Remove the width and height from the element.
+          elem.removeAttribute("width");
+          elem.removeAttribute("height");
+        }
+      }
+    };
+
+    var setElementSrc = function (elem) {
+      // If a valid HTML element is passed to the function.
+      if (elem && elem.attributes) {
+        processElements(elem);
+      }
       // If an element is found.
-      if (imgs) {
+      else if (imgs) {
         // Loop over each element.
-        for (var i = 0; i < imgs.length; i++) {
-          // Create a sources array. This will store objects containing a width, unit and a src.
-          var sources = [];
-          // Create a matches array. This will store source objects matched against the matchMedia.
-          var matches = [];
-          // Create a reference to the current element.
-          var elem = imgs[i];
-          // Loop over each attribute of the element.
-          for (var j = 0; j < elem.attributes.length; j++) {
-            // Create a reference for the current attribute.
-            var elemAttr = elem.attributes[j];
-            // If the current attributes name contains the 'settings.prefix'.
-            if (regex.test(elemAttr.nodeName)) {
-              // Add an object containing width, unit and src and add it to the sources array.
-              sources.push(
-                { "width" : elemAttr.nodeName.replace(settings.prefix, "").replace(/\D/g, ""),
-                  "unit"  : elemAttr.nodeName.replace(settings.prefix, "").replace(/[0-9]/g, ""),
-                  "src"   : elemAttr.value }
-              );
-            }
-          }
-          // Sort the sources array in ascending width order.
-          sources = sources.sort(sortObjectByWidth);
-          // Loop over the source array.
-          for (var k = 0; k < sources.length; k++) {
-            // If matchMedia is supported & matchMedia matches.
-            if (window.matchMedia && window.matchMedia("(min-width: " + sources[k].width + sources[k].unit + ")").matches) {
-              // Add the sources object to the matches array.
-              matches.push(sources[k]);
-            }
-            // If matchMedia is not supported
-            if (!window.matchMedia) {
-              // Add the last sources object to the match array
-              // Break out of the loop
-              matches.push(sources[sources.length - 1]);
-              break;
-            }
-          }
-          // If there is a match.
-          if (matches.length) {
-            // Create a reference to last matched object.
-            var matchedEl = matches.pop();
-            // If the src of the element is NOT the same as the matchedEl.src
-            if (matchedEl.src !== elem.getAttribute("data-src")) {
-              // Set the data-src of the element to the src of the matchedEl object.
-              elem.setAttribute("data-src", matchedEl.src);
-              // Set the element src by calling the resrc.getResrcImageObject public method.
-              elem.src = resrc.getResrcImageObject(elem).resrcImgPath;
-              // Fallback gracefully to the remote image if there is an error.
-              elem.onerror = useFallbackImgPath;
-              // Remove the width and height from the element.
-              elem.removeAttribute("width");
-              elem.removeAttribute("height");
-            }
-          }
+        for (var k = 0; k < imgs.length; k++) {
+          processElements(imgs[k]);
         }
       }
     };
@@ -91,6 +97,9 @@
 
     // Execute the setElementSrc function.
     setElementSrc();
+
+    // Expose setElementSrc function.
+    resrc.setElementSrc = setElementSrc;
 
     // return the resrc object so the public resrc methods can be chained.
     return resrc;
